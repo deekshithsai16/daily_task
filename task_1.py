7151d3351c0a4ada2faa7e87f94a4cb08b64368f
deekshithsai16 <deekshithsai08@cloud.com>
2026-02-17 20:39:19 +0530

    task

---
+++ task_1.py
@@ -0,0 +1,80 @@
+#Write a Python program to print your name, qualification, and goal in three separate lines.
+name_qua_goal='''V Deekshith sai\n
+Btech 1st year\n
+To become a data scientist'''
+print(name_qua_goal)
+
+
+#Print a sentence using single quotes and double quotes.
+sent="VJ INNOVATIVE SOLUTIONS"
+print(sent)
+sing_cout_sent='MITS DEEMED TO BE UNIVERSITY'
+print(sing_cout_sent)
+
+#Print the result of 10 + 20 inside the print() function.
+value_1=10
+value_2=20
+print(value_1+value_2)
+
+#Create variables to store your name, age, and city. Print them in a single output statement.
+st_name='sai'
+st_age=18
+st_city='madanapalle'
+print(st_name,st_age,st_city)
+
+
+#Write 3 valid and 3 invalid variable names (mention why invalid names are incorrect).
+
+#valid variables
+std_name='deekshith'
+clg_name='MITS'
+branch_stud='data_science'
+print(std_name,clg_name,branch_stud)
+
+#invalid variables
+#$_nam='deekshith'
+#@_clg_name='MITS'
+#1_branch stud='data science'
+reason="these are invalid variables because they are starting with symbols numbers"
+print(reason)
+
+#Reassign a new value to an existing variable and print the updated value.
+weight=59
+weight=56	#updated weight (reassign weight)
+print(weight)
+
+#Create one example each for int, float, str, and bool.
+#Use the type() function to check the data type of each variable.
+height=160	#int
+print(type(height))
+print(height)
+
+weight=59.6	#float
+print(type(weight))
+print(weight)
+
+name='deekshith sai'	#string
+print(type(name))
+print(name)
+
+#Convert an integer into a float and print the result.
+roll=22
+print(float(roll))
+
+#Take a string “PYTHONPROGRAM” and:
+prog='PYTHONPROGRAM'
+print(prog[0])
+print(prog[-1])
+print(prog[0:6:1])
+print(prog[-1:-14:-1])
+
+#Create a list of 5 numbers and perform:
+list='innovative'
+print(list[0])
+print(list[-1])
+print(list[0:4:1])
+
+
+
+
+
